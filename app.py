1acc7d942d13257d9f1e5962e141694648a32973
ANURAG-2043 <96662574+ANURAG-2043@users.noreply.github.com>
2023-10-17 23:38:16 +0530

    proper working prj

--- app.py
+++ app.py
@@ -4,20 +4,15 @@ import sklearn
 import pickle
 import pandas as pd
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder='static')
 model = pickle.load(open("flight_rf.pkl", "rb"))
-
-
-
+ 
 @app.route("/")
 @cross_origin()
 def home():
     return render_template("home.html")
-
-
-
-
-@app.route("/predict", methods = ["GET", "POST"])
+  
+@app.route("/", methods = ["GET", "POST"])
 @cross_origin()
 def predict():
     if request.method == "POST":
@@ -26,30 +21,29 @@ def predict():
         date_dep = request.form["Dep_Time"]
         Journey_day = int(pd.to_datetime(date_dep, format="%Y-%m-%dT%H:%M").day)
         Journey_month = int(pd.to_datetime(date_dep, format ="%Y-%m-%dT%H:%M").month)
-        # print("Journey Date : ",Journey_day, Journey_month)
+       
 
         # Departure
         Dep_hour = int(pd.to_datetime(date_dep, format ="%Y-%m-%dT%H:%M").hour)
         Dep_min = int(pd.to_datetime(date_dep, format ="%Y-%m-%dT%H:%M").minute)
-        # print("Departure : ",Dep_hour, Dep_min)
+        
 
         # Arrival
         date_arr = request.form["Arrival_Time"]
         Arrival_hour = int(pd.to_datetime(date_arr, format ="%Y-%m-%dT%H:%M").hour)
         Arrival_min = int(pd.to_datetime(date_arr, format ="%Y-%m-%dT%H:%M").minute)
-        # print("Arrival : ", Arrival_hour, Arrival_min)
+        
 
         # Duration
         dur_hour = abs(Arrival_hour - Dep_hour)
         dur_min = abs(Arrival_min - Dep_min)
-        # print("Duration : ", dur_hour, dur_min)
+        
 
         # Total Stops
         Total_stops = int(request.form["stops"])
-        # print(Total_stops)
 
         # Airline
-        # AIR ASIA = 0 (not in column)
+      
         airline=request.form['airline']
         if(airline=='Jet Airways'):
             Jet_Airways = 1
@@ -207,20 +201,6 @@ def predict():
             Vistara_Premium_economy = 0
             Trujet = 0
 
-        # print(Jet_Airways,
-        #     IndiGo,
-        #     Air_India,
-        #     Multiple_carriers,
-        #     SpiceJet,
-        #     Vistara,
-        #     GoAir,
-        #     Multiple_carriers_Premium_economy,
-        #     Jet_Airways_Business,
-        #     Vistara_Premium_economy,
-        #     Trujet)
-
-        # Source
-        # Banglore = 0 (not in column)
         Source = request.form["Source"]
         if (Source == 'Delhi'):
             s_Delhi = 1
@@ -252,13 +232,6 @@ def predict():
             s_Mumbai = 0
             s_Chennai = 0
 
-        # print(s_Delhi,
-        #     s_Kolkata,
-        #     s_Mumbai,
-        #     s_Chennai)
-
-        # Destination
-        # Banglore = 0 (not in column)
         Source = request.form["Destination"]
         if (Source == 'Cochin'):
             d_Cochin = 1
@@ -301,26 +274,6 @@ def predict():
             d_New_Delhi = 0
             d_Hyderabad = 0
             d_Kolkata = 0
-
-        # print(
-        #     d_Cochin,
-        #     d_Delhi,
-        #     d_New_Delhi,
-        #     d_Hyderabad,
-        #     d_Kolkata
-        # )
-        
-
-    #     ['Total_Stops', 'Journey_day', 'Journey_month', 'Dep_hour',
-    #    'Dep_min', 'Arrival_hour', 'Arrival_min', 'Duration_hours',
-    #    'Duration_mins', 'Airline_Air India', 'Airline_GoAir', 'Airline_IndiGo',
-    #    'Airline_Jet Airways', 'Airline_Jet Airways Business',
-    #    'Airline_Multiple carriers',
-    #    'Airline_Multiple carriers Premium economy', 'Airline_SpiceJet',
-    #    'Airline_Trujet', 'Airline_Vistara', 'Airline_Vistara Premium economy',
-    #    'Source_Chennai', 'Source_Delhi', 'Source_Kolkata', 'Source_Mumbai',
-    #    'Destination_Cochin', 'Destination_Delhi', 'Destination_Hyderabad',
-    #    'Destination_Kolkata', 'Destination_New Delhi']
         
         prediction=model.predict([[
             Total_stops,
@@ -355,14 +308,10 @@ def predict():
         ]])
 
         output=round(prediction[0],2)
-
+        
         return render_template('home.html',prediction_text="Your Flight price is Rs. {}".format(output))
-
-
-    return render_template("home.html")
-
-
-
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
